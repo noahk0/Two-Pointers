45934ecab7b8a83ce9dbf31e679cb9a0deadb263
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-16 19:22:04 +0900

    Create 0medium Two Sum II - Input Array Is Sorted.py

---
+++ 0medium Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,12 @@
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    left, right = 0, len(numbers) - 1
+
+    while left < right:
+        candidate = numbers[left] + numbers[right]
+
+        if candidate < target:
+            left += 1
+        elif target < candidate:
+            right -= 1
+        else:
+            return [left + 1, right + 1]
